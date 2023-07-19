572a75dacc7404186447238165f199ce08c06a6c
Stephany18100 <133921239+Stephany18100@users.noreply.github.com>
2023-07-19 16:27:20 -0500

    dfef

---
+++ Proyecto_Gestion/Entities/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Gestion.Entities
+{
+    public class Empleaado
+    {
+  
+     public int cod_empleado { get; set; }
+        public string nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string Email { get; set; }
+        public int salario { get; set; }
+    }
+}
